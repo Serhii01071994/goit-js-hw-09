8a19ccc950afd001f22458496aa5ebe1c18e8c62
Serhii01071994 <126762718+Serhii01071994@users.noreply.github.com>
2023-08-05 00:16:11 +0300

    1 задача готова

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+let interval;
+
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+
+btnStart.addEventListener('click', () => {
+  if (!interval) {
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+
+    interval = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  }
+});
+
+btnStop.addEventListener('click', () => {
+  if (interval) {
+    clearInterval(interval);
+    interval = null;
+
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+  }
+});
